6e431424fe3a338eab1615f9bbb93826365b863a
EduardoCastilhos <eduardocastilhos2005@hotmail.com>
2025-04-14 19:19:24 -0300

    Foi adicionado as funcionalidades de tarefa

--- public/index.php
+++ public/index.php
@@ -1,9 +1,25 @@
 <?php
-// Inicia a sessão no navegador
 
-// Insira aqui os includes e/ou requires
+    ini_set("display_errors", 1);
+    ini_set("display_startup_errors", 1);
+    error_reporting(E_ALL);
 
-// Processa as requisições do navegador
+    // Inicia a sessão no navegador
 
-//exibir a lista de tarefas
+    session_start();
+
+    // Insira aqui os includes e/ou requires
+
+    require_once __DIR__ . '/../src/controllers/controler.php';
+
+    // Processa as requisições do navegador
+
+    if($_SERVER['RESQUEST_METHOD'] == 'POST'){
+        handleRequest();
+    }
+
+    //exibir a lista de tarefas
+
+    showTasks();
+    
 ?>
